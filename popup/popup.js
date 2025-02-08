6104579d2122c452c675145aaaa3ffd2c6d58328
H0541yomi <ejcabalona727@gmail.com>
2025-02-08 16:40:52 -0500

    t

--- popup/popup.js
+++ popup/popup.js
@@ -2,18 +2,18 @@ const statusTagElement = document.getElementById("statusTag");
 const inactiveTagElement = document.getElementById("inactiveTag");
 const activeTagElement = document.getElementById("activeTag");
 
-    chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
-        const headers = new Headers({
-            'Authorization' : 'Bearer ' + token,
-            'Content-Type': 'application/json'
-        })
-      
-        const queryParams = { headers };
-      
-        fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
-        .then((response) => response.json()) // Transform the data into json
-        .then(function(data) {
-            console.log(data);
-          })
-        })
+document.getElementById('signin').addEventListener('click', () => {
+chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+    const headers = new Headers({
+        'Authorization' : 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    })
   
+    const queryParams = { headers };
+  
+    fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+    .then((response) => response.json()) // Transform the data into json
+    .then(function(data) {
+        console.log(data);
+      })
+    })})
