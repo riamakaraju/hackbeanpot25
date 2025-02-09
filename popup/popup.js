fcc152b48d118fc44040b144322c3f557c3eb1e4
ding-blip <ding.chl@northeastern.edu>
2025-02-08 19:42:50 -0500

    updated toggle

--- popup/popup.js
+++ popup/popup.js
@@ -48,26 +48,26 @@ const updateUI = (isRunning) => {
     }
 }
     
-chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
 document.getElementById('signin').addEventListener('click', () => {
 
 
-chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-    const headers = new Headers({
-        'Authorization' : 'Bearer ' + token,
-        'Content-Type': 'application/json'
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        const headers = new Headers({
+            'Authorization' : 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        })
+    
+       console.log(token);
+      
+        const queryParams = { headers };
+      
+        fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+        .then((response) => response.json()) // Transform the data into json
+        .then(function(data) {
+            console.log(data);
+          })
+    
+    
+        })
     })
-
-   console.log(token);
-  
-    const queryParams = { headers };
-  
-    fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
-    .then((response) => response.json()) // Transform the data into json
-    .then(function(data) {
-        console.log(data);
-      })
-
-
-    })
-})
+    
